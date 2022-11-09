87fa977e7f0e2ae92b2d19895937adfae48a37ab
Temótio Luis Bernardo <timotio.bernardo@snowmanlabs.com>
2022-11-09 15:13:26 -0300

    Estruturando o projeto.

---
+++ src/pages/loginUser.jsx
@@ -0,0 +1,9 @@
+import { Button } from '@mui/material';
+export const LoginUser = () => {
+    return (
+        <>
+            <Button color="secondary">Secondary</Button>
+            <Button variant="contained" color="success" />
+        </>
+    )
+}
