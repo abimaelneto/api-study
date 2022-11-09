eb4a53ed3373a3ef6904c4d2d9379a01856c2d8d
Temótio Luis Bernardo <timotio.bernardo@snowmanlabs.com>
2022-11-09 15:27:32 -0300

    estruturando o projeto

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom"
 import './App.css'
+
+import { HomePage } from "./pages/Home/Home"
 import { LoginUser } from "./pages/loginUser"
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<LoginUser />}>
+        <Route path="/" element={<HomePage />}>
         </Route>
       </Routes>
     </BrowserRouter>
