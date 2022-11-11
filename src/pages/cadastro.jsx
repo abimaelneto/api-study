367affc2cb52454f63b7769791e3ceb038f6a5e9
quizitocristiano <quizitocristiano10@gmail.com>
2022-11-11 16:05:06 -0300

    modificando o cadastro

--- src/pages/cadastro.jsx
+++ src/pages/cadastro.jsx
@@ -6,27 +6,29 @@ import "../pages/cadastro.css";
 export const Cadastro = () => {
   return (
     <>
-      <div className="containerlogin">
-        <div className="container-login" data-aos="flip-left">
-          <div className="div-lubi">
-            <div className="wrappr-login">
-              <div className="digite">
-                <p>Para garantir a sua segurança faça o seu Cadastro</p>
-              </div>
-              <div className="my-wrapper-input">
+      <div className="container-login" data-aos="flip-left">
+        <div className="div-lubi">
+          <div className="wrappr-login">
+            <div className="digite">
+              <p className="tex-p">
+                Para garantir a sua segurança faça o seu cadastro
+              </p>
+            </div>
+            <div className="my-wrapperinput">
+              <div className="info-input-1">
                 <div className="input-wrappre">
                   <input
-                    name="n"
+                    name="Nome"
                     type="text"
                     required="required"
                     autoComplete="false"
                   />
-                  <span>nome</span>
+                  <span>Nome</span>
                   <i></i>
                 </div>
                 <div className="input-wrappre">
                   <input
-                    name="email"
+                    name="E-mail"
                     type="text"
                     required="required"
                     autoComplete="false"
@@ -34,26 +36,58 @@ export const Cadastro = () => {
                   <span>E-mail</span>
                   <i></i>
                 </div>
-
                 <div className="input-wrappre">
-                  <input name="password" type="pessword" required="required" />
-                  <span>password</span>
+                  <input
+                    name="Telefone"
+                    type="text"
+                    required="required"
+                    autoComplete="false"
+                  />
+                  <span>Telefone</span>
                   <i></i>
                 </div>
               </div>
-              <div className="mybotton">
-                <button type="botao" className="cadastro">
-                  Cadastro
-                </button>
-
-                <a className="linkeparacadastro" href="#">
-                  <span className="spanlinke"></span>
-                  <span className="spanlinke"></span>
-                  <span className="spanlinke"></span>
-                  <span className="spanlinke"></span>
-                  já tenho uma conta!
-                </a>
+              <div className="info-input-2">
+                <div className="input-wrappre">
+                  <input name="password" type="pessword" required="required" />
+                  <span>Password</span>
+                  <i></i>
+                </div>
+                <div className="input-wrappre">
+                  <input
+                    name="Confirmação de senha"
+                    type="pessword"
+                    required="required"
+                  />
+                  <span>Confirmação de senha</span>
+                  <i></i>
+                </div>
+                <div className="input-wrappre">
+                  <input
+                    name="Data De Nascimento"
+                    type="Data De Nascimento"
+                    required="Data De Nascimento"
+                  />
+                  <span>Data De Nascimento</span>
+                  <i></i>
+                </div>
               </div>
+            </div>
+            <div className="mybotton">
+              <button className="a-dobotaoa" href="#">
+                <span className="spanbotao"></span>
+                <span className="spanbotao"></span>
+                <span className="spanbotao"></span>
+                <span className="spanbotao"></span>
+                cadastrar
+              </button>
+              <a className="linkeparacadastro" href="#">
+                <span className="spanlinke"></span>
+                <span className="spanlinke"></span>
+                <span className="spanlinke"></span>
+                <span className="spanlinke"></span>
+                já tenho uma conta!!
+              </a>
             </div>
           </div>
         </div>
