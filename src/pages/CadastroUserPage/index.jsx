da3b36a655b45e7a529374e5973518666d8660d6
Temótio Luis Bernardo <timotio.bernardo@snowmanlabs.com>
2022-11-18 17:18:41 -0300

    alterando os conponentes e dando um pouco de aula.

--- src/pages/CadastroUserPage/index.jsx
+++ src/pages/CadastroUserPage/index.jsx
@@ -1,19 +1,51 @@
+import { AccessAlarm, AccountCircle, Visibility, VisibilityOff } from '@mui/icons-material'
+import { Box, Button, Checkbox, FormControl, FormControlLabel, FormGroup, Input, InputAdornment, InputLabel, Stack } from '@mui/material'
+import { useState } from 'react'
+import { Password } from '../../components/PasswordInput'
 import './index.css'
 
 import { InputesList } from './INPUTS'
 
 export const CadastroUser = () => {
+    const [aceitouTermos, setAceitouTermos] = useState(false)
+
+    const handleTermos = () => {
+        setAceitouTermos(!aceitouTermos)
+    }
+    const handleCadastrar = () => {
+
+    }
+
     return (
         <>
             <div className="container">
                 <h1 className="cad-h1" style={{ color: 'green' }}>
                     Registrar usuarios
                 </h1>
-                <div className="itens_center">
-                    <div className="center_left">
-                        <InputesList/>
-                    </div>
-                </div>
+                <Stack className="form-cadastro"  >
+                    <InputesList/>
+
+                    <FormGroup>
+                        <FormControlLabel control={<Checkbox onClick={handleTermos} />} label="Aceito termos e condições" />
+                    </FormGroup>
+                    
+                    <Stack>
+                   
+                            <Button onClick={handleCadastrar} variant="contained" color="primary" disabled={!aceitouTermos}>
+                                Cadastrar
+                            </Button>
+                     
+                    </Stack>
+                  <Password label="Password do Quizito" iconShow={<AccountCircle/>} iconNotShow={<AccessAlarm/>}/>
+                  <Password label="Password do Temotio"/>
+
+                  <Password label="Senha 1 "/>
+
+                  <Password label="Senha 2"/>
+
+                  <Password label="Password do Quizito"/>
+
+                </Stack>
             </div>
         </>
     )
