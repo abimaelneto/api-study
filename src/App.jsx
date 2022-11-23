b4d5dfd818c83bcea1540867b0a2d4d7b540ec6a
Temótio Luis Bernardo <timotio.bernardo@snowmanlabs.com>
2022-11-23 14:23:07 -0300

    Apaga route cadastro

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,12 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./App.css";
 import { Login } from "./pages/indexlogin";
-import { Cadastro } from "./pages/cadastro"
+
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Login />} />
-        <Route path="/xasdasdas" element={<Cadastro />} />
       </Routes>
     </BrowserRouter>
   );
