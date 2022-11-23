0c235576f3fdaeb1c29d82bc066ad58c553bc02a
Abimael Neto <abimael.oliveiraneto@gmail.com>
2022-11-23 08:41:43 -0300

    usa tema no app

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,19 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./App.css";
-import { CadastroUser} from "./pages/CadastroUserPage/index"
+import { ThemeProvider } from "./contexts/theme";
+import { CadastroUser } from "./pages/CadastroUserPage/index";
 import { Dashboard } from "./pages/Dashboard/index";
-import { HomePage } from "./pages/Home/Home"
+import { HomePage } from "./pages/Home/Home";
 function App() {
   return (
     <BrowserRouter>
-      <Routes>
-        <Route path="/cadastro" element={<CadastroUser />} />
-        <Route path="/" element={<Dashboard />} />
-        <Route path="/" element={<HomePage />} />
-      </Routes>
+      <ThemeProvider>
+        <Routes>
+          <Route path="/CadastroUser" element={<CadastroUser />} />
+          <Route path="/" element={<Dashboard />} />
+          <Route path="/" element={<HomePage />} />
+        </Routes>
+      </ThemeProvider>
     </BrowserRouter>
   );
 }
