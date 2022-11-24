f7d076b70bc9408aa0e88461b26427caf8d78367
Temótio Luis Bernardo <timotio.bernardo@snowmanlabs.com>
2022-11-24 13:37:31 -0300

    conponentizando o index do projeto

---
+++ src/pages/Home/Home.jsx
@@ -0,0 +1,13 @@
+import './Home.css'
+import { AnimationPage } from './animation'
+export const HomePage = () => {
+    return (
+        <>
+            <AnimationPage
+                title="Log in or Register"
+                buttontitle="Log in"
+                buttontitle1="Register"
+            />
+        </>
+    )
+}
