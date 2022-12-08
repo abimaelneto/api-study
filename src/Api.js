cf8b82ead7be886b049c1f7324a940de17478380
Temótio Luis Bernardo <timotio.bernardo@snowmanlabs.com>
2022-12-08 14:34:24 -0300

    renomea as keys de getPessoas na Api

---
+++ src/Api.js
@@ -0,0 +1,36 @@
+export default {
+    getPessoas: () => [
+        {
+            nome: 'Fulano de Tall Mario',
+            email: 'fulanodetall32@gmail.com',
+            celphone: 41997610666,
+            birthDate : '10/20/2022',
+            registrationDate: '10/10/1999',
+            managementLevel: 'beginner',
+        },
+        {
+            nome: 'Salvador',
+            email: 'salvador@gmail.com',
+            celphone: 43565867858,
+            birthDate : '10/20/22',
+            registrationDate: '10/29/2000',
+            managementLevel: 'vice-admin',
+        },
+        {
+            nome: 'Agentes da Empresa TI',
+            email: 'agentesti003@gmail.com',
+            celphone: 41003,
+            birthDate : '10/20/22',
+            registrationDate: '02/10/2014',
+            managementLevel: 'Admin',
+        },
+        {
+            nome: 'Emergência de Verdade',
+            email: 'emergênciadeverdade@gmail.com',
+            celphone: 155,
+            birthDate : '01/01/1978',
+            registrationDate: '02/04/2001',
+            managementLevel: 'full access'
+        }
+    ]
+}
