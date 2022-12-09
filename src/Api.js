c4ce02acd16a58d1396e987f5304fffd81310d2d
Temótio Luis Bernardo <timotio.bernardo@snowmanlabs.com>
2022-12-09 16:42:33 -0300

    muda os campos nomes para username, e add

--- src/Api.js
+++ src/Api.js
@@ -1,36 +1,44 @@
 export default {
     getPessoas: () => [
         {
-            nome: 'Fulano de Tall Mario',
+            username: 'Fulano de Tall Mario',
             email: 'fulanodetall32@gmail.com',
             celphone: 41997610666,
             birthDate : '10/20/2022',
             registrationDate: '10/10/1999',
             managementLevel: 'beginner',
+            password: 'camotio32',
+            passwordConfirmation: 'camotio32'
         },
         {
-            nome: 'Salvador',
+            username: 'Salvador',
             email: 'salvador@gmail.com',
             celphone: 43565867858,
             birthDate : '10/20/22',
             registrationDate: '10/29/2000',
             managementLevel: 'vice-admin',
+            password: '00000000',
+            passwordConfirmation: '00000000'
         },
         {
-            nome: 'Agentes da Empresa TI',
+            username: 'Agentes da Empresa TI',
             email: 'agentesti003@gmail.com',
             celphone: 41003,
             birthDate : '10/20/22',
             registrationDate: '02/10/2014',
             managementLevel: 'Admin',
+            password: '12345678',
+            passwordConfirmation: '12345678'
         },
         {
-            nome: 'Emergência de Verdade',
+            username: 'Emergência de Verdade',
             email: 'emergênciadeverdade@gmail.com',
             celphone: 155,
             birthDate : '01/01/1978',
             registrationDate: '02/04/2001',
-            managementLevel: 'full access'
+            managementLevel: 'full access',
+            password: '11223344',
+            passwordConfirmation: '11223344'
         }
     ]
 }
