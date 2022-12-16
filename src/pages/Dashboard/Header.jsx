932907a3d09eda2e44c9fa537aa748d111bc9caa
Temótio Luis Bernardo <timotio.bernardo@snowmanlabs.com>
2022-12-16 14:42:53 -0300

    Add icon config no header

--- src/pages/Dashboard/Header.jsx
+++ src/pages/Dashboard/Header.jsx
@@ -1,15 +1,32 @@
 import "./Header.css";
-export const Header = () => {
+import SettingsSuggestIcon from '@mui/icons-material/SettingsSuggest';
+export const Header = ({ option }) => {
   return (
     <>
       <div className="containerLogoUserLoged">
-        <div className="mini-info-userLoged">
-          <h3 className="salutation">
-            <strong>O</strong>
-            <strong>l</strong>
-            <strong>a</strong>!
-          </h3>
-          <p>Agente da Empresa</p>
+        <div style={{
+          display: 'flex',
+          alignItems: 'center',
+          justifyContent: 'space-between',
+          paddingLeft: '1rem',
+          paddingRight: '1rem'
+        }} className="mini-info-userLoged">
+          <div style={{
+            display: 'flex',
+            alignItems: 'center',
+            justifyContent: 'center',
+            gap: '5px'
+          }}>
+            <h3 className="salutation">
+              <strong>O</strong>
+              <strong>l</strong>
+              <strong>a</strong>!
+            </h3>
+            <p>Agente da Empresa</p>
+          </div>
+          <div className="setting">
+            <SettingsSuggestIcon onClick={option} />
+          </div>
         </div>
         <div className="userLoged-office">
           <h3>Management level:</h3>
